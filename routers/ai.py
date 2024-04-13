8ea6ba8fc2b257518c15e11166e6d0c61eb7f11a
EtriKodoku <volodymyrpetriv2207@gmail.com>
2024-04-13 19:40:33 +0300

    Updated token. Updated system prompt

--- routers/ai.py
+++ routers/ai.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 from anthropic import Anthropic
 
 router = APIRouter(prefix="/ai")
-SYSTEM_PROMPT = "You are an psychologist AI assistant named Віра"
+SYSTEM_PROMPT = "You are a psychological AI assistant named Віра. After greeting introduce yourself to user. You need to ask user about different parts of his psychological condition. Don't repeat questions. Just ask next question after you get user answer, don't provide any suggestions. Your time is limited so you need cover as much as you can for least questions. You will speak with user in Ukrainian. User can ask you for a report about his psychological state. Your report is for psychologist, you have to provide only user condition without any suggestions about healing"
 
 @router.websocket('/wschat')
 async def wscaht(websocket: WebSocket, ai: Anthropic = Depends(get_ai)):
@@ -21,7 +21,8 @@ async def wscaht(websocket: WebSocket, ai: Anthropic = Depends(get_ai)):
             return
         chat_log.append({"role": "user", "content": recieved["text"]})
         msg_buf = ""
-        with ai.messages.stream(max_tokens=1024, messages=chat_log, model="claude-3-sonnet-20240229", system=SYSTEM_PROMPT) as stream:
+        print(chat_log)
+        with ai.messages.stream(max_tokens=1024, messages=chat_log, model="claude-3-haiku-20240307", system=SYSTEM_PROMPT) as stream:
             for text in stream.text_stream:
                 msg_buf += text
                 await websocket.send_json({"role": "part", "text": text})
