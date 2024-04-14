6681f01c373f5467491e71fddc1c06d14bf26659
EtriKodoku <volodymyrpetriv2207@gmail.com>
2024-04-14 03:33:13 +0300

    Staying alive, staying alive. a-a-a Goodnight

--- models.py
+++ models.py
@@ -6,3 +6,11 @@ class LoginUser(BaseModel):
     class Config:
         orm_mode = True
         extra = Extra.forbid
+
+
+class Report(BaseModel):
+    text: str
+    rep_type: str
+    
+    class Config:
+        orm_mode = True
--- routers/ai.py
+++ routers/ai.py
@@ -1,9 +1,7 @@
-import secrets
-from mail import make_message, send_conf_mail_to
 from db import get_db
 from ai import get_ai
-from models import LoginUser
-from fastapi import APIRouter, Depends, Request, WebSocket
+from models import Report
+from fastapi import APIRouter, Depends, WebSocket
 from pymongo import MongoClient
 from anthropic import Anthropic
 
@@ -11,11 +9,11 @@ router = APIRouter(prefix="/ai")
 SYSTEM_PROMPT = "You are a psychological AI assistant named Віра that analyzes user condition and provide the best review for professional psychological support. After greeting introduce yourself to user. You need to ask user about different parts of his psychological condition. Don't repeat questions. Just ask next question after you get user answer, don't provide any suggestions. Your question shouldn't be dry. Your time is limited so you need cover as much as you can for least questions. You will speak with user in Ukrainian. User can ask you for a report about his psychological state. Your report is for psychologist, you have to provide only user condition without any suggestions about healing"
 
 @router.websocket('/wschat')
-async def wscaht(websocket: WebSocket, ai: Anthropic = Depends(get_ai)):
+async def wscaht(websocket: WebSocket, ai: Anthropic = Depends(get_ai), db = Depends(get_db)):
     await websocket.accept()
     chat_log = []
     i = 0       # Index of answer
-    limit = 3   # Maximum number of answers
+    limit = 1   # Maximum number of answers
     while True:
         i += 1
         recieved = await websocket.receive_json()
@@ -34,13 +32,14 @@ async def wscaht(websocket: WebSocket, ai: Anthropic = Depends(get_ai)):
                 msg_buf += text
                 await websocket.send_json({"role": "part", "text": text})
         chat_log.append({"role": "assistant", "content": msg_buf})
-        
+
         if i == limit:
             content = content + ". Жодних запитань більше не став. Сформуй звіт для психологів. Надішли мені сухий звіт"
             chat_log.append({"role": "user", "content": content})
             with ai.messages.stream(max_tokens=1024, messages=chat_log, model="claude-3-haiku-20240307", system=SYSTEM_PROMPT) as stream:
                 for text in stream.text_stream:
                     msg_buf += text
+                print(db.сodecon.reports.insert_one({'text': text, 'rep_type':'Psych'}))
                 await websocket.send_json({"role": "end", "text": msg_buf})
             break
         await websocket.send_json({"role": "finished", "text": msg_buf})
