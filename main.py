e5b9649580b33ee305fc273b4c6d98328ad9cb86
Igor4er <sherednihenko.ihor@gmail.com>
2024-04-14 12:40:37 +0300

    asdasd

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from config import CONFIG
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="app:app", host="0.0.0.0", port=433, reload=True, ssl_certfile=CONFIG.ssl_certfile, ssl_keyfile=CONFIG.ssl_keyfile)
+    uvicorn.run(app="app:app", host="0.0.0.0", reload=True, ssl_certfile=CONFIG.ssl_certfile, ssl_keyfile=CONFIG.ssl_keyfile)
